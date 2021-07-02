ee2028e6d05e8e7b990b8e7ef4f22586686e8ea4
xiangqing <xiangqing@ainnovation.com>
2021-07-02 14:54:03 +0800

    change reader's para from tuple to dict.

--- data_transforms/transforms.py
+++ data_transforms/transforms.py
@@ -6,50 +6,46 @@
 from tqdm import tqdm
 
 from data_transforms.get_cls_from_name import get_r_cls, get_w_cls, get_p_cls
+from data_transforms.processor.processor import EmptyProcess
 from data_transforms.reader.reader import ConcatReader
 from data_transforms.writer.writer import ConcatWriter
-from data_transforms.processor.processor import EmptyProcess
 
 '''
 content:
     img:
     info:
-        imageName: os.path.split(json_cont["imagePath"])[-1]
-        imageWidth: json_cont["imageWidth"]
-        imageHeight: json_cont["imageHeight"]
+        imageName:
+        imageWidth:
+        imageHeight:
         imageDepth: 
+        shapes:[
+            shape_type: polygon | box
+            points
+        ]
 '''
 
 
 class DataTransforms(object):
     def __init__(self,
-                 reader_method, reader_path,
-                 writer_method, writer_path,
-                 processor=None,
+                 reader_method, writer_method,
+                 processor=None, is_recursive=False,
                  ):
-        self.reader = self._get_reader(reader_method, reader_path)
-        self.writer = self._get_writer(writer_method, writer_path)
+        self.reader = self._get_reader(reader_method)
+        self.writer = self._get_writer(writer_method)
         self.processor = self._get_processor(processor)
+        self.is_recursive = is_recursive
 
-    def _get_reader(self, reader_method, reader_path):
+    def _get_reader(self, reader_method):
         readers = []
-        if not isinstance(reader_method, (list, tuple)):
-            reader_method = (reader_method,)
-        if not isinstance(reader_path, (list, tuple)):
-            reader_path = (reader_path,)
-        for rm, rp in zip(reader_method, reader_path):
+        for rm, rp in reader_method.items():
             if isinstance(rm, str):
                 rm = get_r_cls(rm)
             readers.append(rm(rp))
-        return ConcatReader(readers)
+        return ConcatReader(readers, self.is_recursive)
 
-    def _get_writer(self, writer_method, writer_path):
+    def _get_writer(self, writer_method):
         writers = []
-        if not isinstance(writer_method, (list, tuple)):
-            writer_method = (writer_method,)
-        if not isinstance(writer_path, (list, tuple)):
-            writer_path = (writer_path,)
-        for wm, wp in zip(writer_method, writer_path):
+        for wm, wp in writer_method.items():
             if isinstance(wm, str):
                 wm = get_w_cls(wm)
             writers.append(wm(wp))
@@ -57,10 +53,10 @@ class DataTransforms(object):
 
     def _get_processor(self, processor):
         if isinstance(processor, str):
-            processor = get_p_cls(processor)
+            processor = get_p_cls(processor)()
         if processor is None:
-            processor = EmptyProcess
-        return processor()
+            processor = EmptyProcess()
+        return processor
 
     def apply(self):
         for content in tqdm(self.reader):
@@ -69,10 +65,13 @@ class DataTransforms(object):
 
 
 if __name__ == '__main__':
-    transforms = DataTransforms(reader_method="image",
-                                reader_path="test_imgs/inputs/img_or_xml/img/",
-                                writer_method="image",
-                                writer_path="test_imgs/outputs/img_xml_2_img_vis/",
-                                processor="jpg2png",
+    transforms = DataTransforms(reader_method={"image": "/home/xq/文档/projects/日联/data",
+                                               "xml": "/home/xq/文档/projects/日联/data",
+                                               },
+                                writer_method={"image": "/home/xq/文档/projects/日联/data_jpg",
+                                               "json": "/home/xq/文档/projects/日联/data",
+                                               },
+                                processor="crop",
+                                is_recursive=False,
                                 )
     transforms.apply()
