49fcb0918f43a99365668b068d03db98f2794f48
xiangqing <xiangqing@ainnovation.com>
2022-03-21 13:34:59 +0800

    xq finally.

---
+++ data_transformation/example.py
@@ -0,0 +1,57 @@
+from data_transformation import DataTransforms
+from data_transformation.processor import Anno2MaskProcessor
+
+'''
+content:
+    image:
+    info:
+        imageName:
+        imageWidth:
+        imageHeight:
+        imageDepth: 
+        shapes:[
+            {
+                shape_type: "polygon",  # [polygon | bndbox]
+                label: str,
+                points': [(693, 224), (739, 253), ...],  # point1, point2, ...
+            },
+            {
+                'shape_type': "bndbox",
+                'label': 'hand', 
+                'points': [692, 254, 709, 275],  # x_min, y_min, x_max, y_max.
+            },
+            ...
+        ]
+'''
+
+
+class GrayProcessor:
+    def process(self, content):
+        """do some thing"""
+        return content
+
+
+class ModelResult:
+    def __init__(self):
+        self.model = ...
+
+    def process(self, content):
+        res = self.model.infer(content["image"])
+        content["info"] = ...(res)
+        return content
+
+
+if __name__ == '__main__':
+    label_val_dict = {"qikong": 1, "shusong": 2}
+    transforms = DataTransforms(
+        reader_method={
+            "image": "/run/user/1000/gvfs/smb-share:server=10.18.103.158,share=share/项目/无损探伤/数据/现场采集/2021-07-19/气孔tiff",
+            # "xml": "/run/user/1000/gvfs/smb-share:server=10.18.103.158,share=share/项目/无损探伤/数据/标注/2021-07-12"
+            },
+        writer_method={
+            "image": "/run/user/1000/gvfs/smb-share:server=10.18.103.158,share=share/项目/无损探伤/数据/现场采集/2021-07-19/气孔jpg",
+            # "xml": "../test_imgs/outputs/xml/"
+            },
+        processor="img2jpg",  # Anno2MaskProcessor(label_val_dict=label_val_dict),
+    )
+    transforms.apply()
---
+++ imgs2video.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/1/6
+# @Author  : xq
+
+
+# -*- coding: UTF-8 -*-
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
+from typing import *
+from xml.dom.minidom import parse
+from PIL import ImageFont, ImageDraw, Image
+from collections import defaultdict
+
+
+__all__ = ["Image2Video"]
+
+
+def add_text(img, text, test_size, location, color):
+    font_path = "/usr/share/fonts/opentype/noto/NotoSerifCJK-SemiBold.ttc"
+    b, g, r = color
+    a = 0
+    font = ImageFont.truetype(font_path, test_size)
+    img_pil = Image.fromarray(img)
+    draw = ImageDraw.Draw(img_pil)
+    draw.text(location, text, font=font, fill=(b, g, r, a))
+    img = np.array(img_pil)
+    return img
+
+
+class Image2Video(object):
+    def __init__(self,
+                 img_path: Text,
+                 video_path: Text,
+                 video_fps: int,
+                 test_color=(0, 0, 255)):
+        self.img_path = img_path
+        self.video_path = video_path
+        self.video_fps = video_fps
+        self.text_color = test_color
+
+        self.img_size = self.get_img_size()
+        self.location = (int(self.img_size[0] * 0.7), int(self.img_size[1] * 0.1))
+        self.obj_num = []
+        self.color_list = [(0, 255, 0),
+                           (50, 255, 50),
+                           (50, 20, 255),
+                           ]
+        self.color_dic = {}
+
+    def get_img_size(self):
+        file_list = sorted(os.listdir(self.img_path))
+        img0 = cv2.imread(os.path.join(self.img_path, file_list[0]))
+        return img0.shape[:2][::-1]
+
+    def _ol_xml_2_mask(self,
+                       xml_path: Text,
+                       img: np.ndarray):
+        xml_file = parse(xml_path)
+        annotation = xml_file.documentElement
+        object_list = annotation.getElementsByTagName("object")
+        current_obj_num = defaultdict(int)
+        for obj in object_list:
+            name = obj.getElementsByTagName("name")[0].childNodes[0].data
+            if name not in self.obj_num:
+                self.obj_num.append(name)
+            current_obj_num[name] += 1
+            if name in self.color_dic:
+                color = self.color_dic[name]
+            elif self.color_list:
+                color = self.color_list[0]
+                self.color_list.pop(0)
+            else:
+                color = np.random.randint(0, 256, 3)
+            self.color_dic[name] = color
+
+            polygon = obj.getElementsByTagName("polygon")[0]
+            pt_list = polygon.getElementsByTagName("pt")
+            point_list = []
+            for pt in pt_list:
+                x = pt.getElementsByTagName("x")[0].childNodes[0].data
+                y = pt.getElementsByTagName("y")[0].childNodes[0].data
+                point_list.append([int(float(x)), int(float(y))])
+            p1, p2 = point_list[0], point_list[2]
+            cv2.rectangle(img, (p1[0], p1[1], p2[0] - p1[0], p2[1] - p1[1]), color, 1)
+        return img
+
+    def apply(self):
+        file_list = sorted(os.listdir(self.img_path))
+        save_path = os.path.join(self.video_path, "video" + ".mp4")
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        video = cv2.VideoWriter(save_path, fourcc, self.video_fps, self.img_size)
+
+        for file_name in tqdm(file_list):
+            # if not file_name.endswith('.PNG'):
+            #     continue
+            path_img = os.path.join(self.img_path, file_name)
+            img = cv2.imread(path_img)
+            video.write(img)
+        return video
+
+
+def main():
+    img_path = "/home/xq/文档/projects/加油站_解决方案/solution_task/run/cfg_wander_gpu_2022-01-07_11-24-40/check_res"
+    video_path = "/home/xq/文档/projects/加油站_解决方案/solution_task/run/cfg_wander_gpu_2022-01-07_11-24-40"
+    fps = 50
+    img2vdo = Image2Video(img_path, video_path, fps)
+    res_video = img2vdo.apply()
+    res_video.release()
+
+
+if __name__ == "__main__":
+    main()
+
